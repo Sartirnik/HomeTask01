49f5c9845f18862b5d01fd301e16a4792c7d04ff
SuprimTrasher <santexnik86@bk.ru>
2025-09-16 14:22:53 +0300

    HomeTask01

--- src/video/handlers/videoHandlers.ts
+++ src/video/handlers/videoHandlers.ts
@@ -4,6 +4,7 @@ import type {Request, Response} from 'express'
 export const createVideoHandler = (req: Request, res: Response) => {
 
     // TODO: добавить обработку видео
+
 }
 
 export const getVideoByIdHandler = (req: Request, res: Response) => {
