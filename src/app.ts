630ee0b7fe5a3838686eef6a854294d18d39b084
SuprimTrasher <santexnik86@bk.ru>
2025-09-21 13:16:06 +0300

    HomeTask01

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ export const app = express();
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('working!!!').status(200);
+    res.send('working!').status(200);
 })
 
 app.use('/videos', VideoRouter)
